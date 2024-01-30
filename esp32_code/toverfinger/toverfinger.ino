f6f27e542e3f9a6ae390285a20c80bd0dfaa16b4
tom <t.a.stuurman@st.hanze.nl>
2024-01-30 13:23:05 +0100

    Update toverfinger.ino

--- esp32_code/toverfinger/toverfinger.ino
+++ esp32_code/toverfinger/toverfinger.ino
@@ -3,8 +3,8 @@
 #include "painlessMesh.h"
 #include <Arduino_JSON.h>
 
-#define MESH_PREFIX     "Mendel-University-mesh"
-#define MESH_PASSWORD   "2*eenbanaanineenBOOT"
+#define MESH_PREFIX     "CircuitCraft-Wifi"
+#define MESH_PASSWORD   ""
 #define MESH_PORT       5555
 
 Adafruit_BME280 bme;
@@ -13,8 +13,6 @@ String readings;
 
 Scheduler userScheduler;
 painlessMesh mesh;
-
-
 
 String getReadings() {
   JSONVar jsonReadings;
@@ -30,11 +28,15 @@ void sendMessage() {
   String msg = getReadings();
   
   // Replace the following line with the IP address of your Raspberry Pi
-  IPAddress piIPAddress(10, 42, 0, 1);
+  IPAddress piIPAddress(10, 80, 93, 5);
   
   mesh.sendSingle(piIPAddress, msg);
 }
+
+
+
 Task taskSendMessage(TASK_SECOND * 5, TASK_FOREVER, &sendMessage);
+
 void initBME() {
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
@@ -63,14 +65,15 @@ void setup() {
   Serial.begin(115200);
 
   initBME();
+  uint8_t externalIP[] = {10,42,0,1};
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION | REMOTE | SYNC | GENERAL | COMMUNICATION | MSG_TYPES | DEBUG);
 
-  mesh.setDebugMsgTypes(ERROR | STARTUP);
   mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-
+  mesh.stationManual(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, externalIP);
   userScheduler.addTask(taskSendMessage);
   taskSendMessage.enable();
 }
